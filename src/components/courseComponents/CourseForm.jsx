f1b96fcbe667529499b8f0d076d81413b065748d
shailendrasingh189 <shailendrasinghpanwar189@gmail.com>
2025-01-25 16:18:53 +0530

    adjust course fee and discount field

--- src/components/courseComponents/CourseForm.jsx
+++ src/components/courseComponents/CourseForm.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addData, updateUser } from "../../redux-config/CourseSlice";
@@ -6,43 +5,46 @@ import axios from "axios";
 
 const CourseForm = ({ toggleModel, editCourse }) => {
   const [formData, setFormData] = useState({
-   name: "",
+    name: "",
     category: "",
     fee: "",
-   courseDuration:""
+    courseDuration: "",
   });
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (editCourse) {
-      setFormData(editCourse); 
+      setFormData(editCourse);
     }
   }, [editCourse]);
-
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-  }
- 
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Submitting data:", formData); 
+    console.log("Submitting data:", formData);
     try {
       if (editCourse) {
-       
-        await axios.put(`http://localhost:9090/course/${editCourse.courseId}`, formData);
+        await axios.put(
+          `http://localhost:9090/course/${editCourse.courseId}`,
+          formData
+        );
         dispatch(updateUser({ ...formData, id: editCourse.courseId }));
       } else {
-      
-        const response = await axios.post("http://localhost:9090/course", formData);
+        const response = await axios.post(
+          "http://localhost:9090/course",
+          formData
+        );
         console.log("API Response:", response.data);
 
         dispatch(addData(response.data));
       }
-  
-      toggleModel(); 
+
+      toggleModel();
     } catch (error) {
       const errorMessage = error.response?.data?.message || error.message;
       console.log(error);
@@ -70,6 +72,7 @@ const CourseForm = ({ toggleModel, editCourse }) => {
               <input
                 type="text"
                 name="name"
+                placeholder="enter course name"
                 value={formData.name}
                 onChange={handleChange}
                 className="block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md"
@@ -77,7 +80,7 @@ const CourseForm = ({ toggleModel, editCourse }) => {
             </div>
 
             <div>
-              <label>Category</label>
+              <label>Course Category</label>
               <select
                 value={formData.category}
                 name="category"
@@ -85,46 +88,63 @@ const CourseForm = ({ toggleModel, editCourse }) => {
                 className="block w-full h-10 py-2 px-4 mt-2 text-gray-700 border rounded-md"
               >
                 <option>Select Category</option>
-                <option value="Job Guaranted Program">Job Guaranted Program</option>
+                <option value="Job Guaranted Program">
+                  Job Guaranted Program
+                </option>
                 <option value="MNC Expert Program">MNC Expert Program</option>
                 <option value="Mastery Program">Mastery Program</option>
                 <option value="Foundation Program">Foundation Program</option>
               </select>
             </div>
 
-            <div>
-              <label>Fee</label>
-              <input
-                type="number"
-                name="fee"
-                value={formData.fee}
-                onChange={handleChange}
-                className="block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md"
-              />
-            </div>
+            <div className="flex justify-between gap-2 ">
+              <div>
+                <label>Course Fee</label>
+                <input
+                  type="number"
+                  name="fee"
+                  placeholder="enter course fee"
+                  value={formData.fee}
+                  onChange={handleChange}
+                  className="block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md"
+                />
+              </div>
 
+              <div>
+                <label>Course Fee Discount</label>
+                <input
+                  type="number"
+                  name="fee"
+                  placeholder="enter fee discount"
+                  value={formData.discount}
+                  onChange={handleChange}
+                  className="block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md"
+                />
+              </div>
+            </div>
             <div>
               <label>Duration</label>
               <input
                 type="text"
                 name="courseDuration"
+                placeholder="enter course duration"
                 value={formData.courseDuration}
                 onChange={handleChange}
                 className="block w-full px-4 py-2 mt-2 bg-white border border-gray-200 rounded-md"
               />
             </div>
           </div>
-          <div className="flex justify-center mx-auto my-2 pb-3">
+          <div className="flex justify-center mx-auto mt-4 my-2 pb-3">
             <button
               type="submit"
               className="w-full px-12 py-2.5 leading-5 text-black/75 font-medium transition-colors duration-300 transform bg-gradient-to-r rounded-md bg-yellow-400 hover:bg-yellow-400 focus:outline-none focus:bg-yellow-300"
               onClick={handleSubmit}
             >
-             Add Course
+              Add Course
             </button>
           </div>
-          <hr />
-
+          {/* <hr /> */}
+{/* 
           <div className="flex justify-end mt-6 pb-3">
             <div className="flex justify-between items-center gap-2">
               <button
@@ -135,7 +155,7 @@ const CourseForm = ({ toggleModel, editCourse }) => {
                 Cancel
               </button>
             </div>
-          </div>
+          </div> */}
         </form>
       </div>
     </section>
