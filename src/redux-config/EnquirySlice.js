237da235c6867102ae708f146a1843496a5e4302
tiya rajput <tiyarajput657@gmail.com>
2025-01-25 12:55:22 +0530

    enquiry functionality and course functionality

--- src/redux-config/EnquirySlice.js
+++ src/redux-config/EnquirySlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   allData: [],
-  editUser: { data: "", isEdit: false },
+
 };
 
 const EnquirySlice = createSlice({
@@ -19,23 +19,14 @@ const EnquirySlice = createSlice({
     {
       state.allData=action.payload;
     },
-    setEditUser: (state, action) => {
-      state.editUser = { data: action.payload, isEdit: true };
-    },
+  
     updateUser: (state, action) => {
       const index = state.allData.findIndex((item) => item.id === action.payload.id);
       if (index !== -1) {
         state.allData[index] = action.payload;
       }
     },
-    resetEditUser: (state) => {
-      state.editUser = { data: "", isEdit: false };
-    },
-
-    // loadData: (state) => {
-    //   const data = JSON.parse(localStorage.getItem("enquiryData")) || [];
-    //   state.allData = data;
-    // },
+  
   },
 });
 
